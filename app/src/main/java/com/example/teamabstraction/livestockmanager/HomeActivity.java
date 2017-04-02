b1144fd939550438d64dba05734e2acfc7ba435d
deathcat05 <catherinemeyer44@gmail.com>
2017-04-02 16:16:13 -0700

    Created a ListView for displaying a list of animals. Trying to make it 'clickable'

--- app/src/main/java/com/example/teamabstraction/livestockmanager/HomeActivity.java
+++ app/src/main/java/com/example/teamabstraction/livestockmanager/HomeActivity.java
@@ -1,52 +1,52 @@
 package com.example.teamabstraction.livestockmanager;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+//These are used for the ListView
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class HomeActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class HomeActivity extends AppCompatActivity
+{
+    private ListView animalListView;
+    private ArrayAdapter<String> animalListAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Find the LisView resource
+        animalListView = (ListView) findViewById(R.id.animalList);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Creates and populates a List of animals (May need to import from a database later
+        String[] animals = new String[]{"Sheep", "Goats", "Cows", "Chickens"};
+        ArrayList<String> listOfAnimalsArray = new ArrayList<String>();
+        listOfAnimalsArray.addAll(Arrays.asList(animals));
+
+        animalListAdapter = new ArrayAdapter<String>(this, R.layout.animal_list_text_view, listOfAnimalsArray);
+
+//Set the ArrayAdapter as the ListView's Adapter
+        animalListView.setAdapter(animalListAdapter);
+        //This will display the animal information once the user clicks on it.
+        animalListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+               //Create code to view the list of a specific animal Activity. 
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
+
+
